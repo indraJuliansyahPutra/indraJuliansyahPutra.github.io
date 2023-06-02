7d190c39b01066237ccb686d98c8d3ad0b9d1bb4
indraJuliansyahPutra <mrindrajuliansyahputra10@gmail.com>
2023-06-02 22:02:35 +0700

    Finish

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -88,3 +88,25 @@ jQuery( document ).ready(function( $ ) {
             });
         }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var skillBoxes = document.querySelectorAll('.skill-box');
+  
+    skillBoxes.forEach(function(box) {
+      var skillBtn = box.querySelector('.skill-btn');
+      var subSkills = box.querySelector('.sub-skills');
+  
+      skillBtn.addEventListener('mouseenter', function() {
+        subSkills.style.display = 'block';
+      });
+  
+      box.addEventListener('mouseleave', function() {
+        subSkills.style.display = 'none';
+      });
+    });
+  });
+  
+function redirectToCredential(credentialUrl) {
+    window.location.href = credentialUrl;
+}
+  
